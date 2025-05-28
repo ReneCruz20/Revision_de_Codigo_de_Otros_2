4ffb0f00c626496ae2b386e6c8468dbca8a13c18
Rene Cruz <m144renecruz2041@gmail.com>
2025-05-28 17:21:22 -0600

    fix: Se corrigen algumas lineas de codigo

--- formulario.js
+++ formulario.js
@@ -1,39 +1,51 @@
-var formulario = document.querySelector("#form")
+/* Se cambia la declaracion de variable de var a const */
+const formulario = document.querySelector("form")
 
-formulario.onsubmit = function(e) {
+formulario.onsubmit = function (e) {
+  /* Se añade Default en el elemento */
+  e.preventDefault();
+  /* Se cambia la declaracion de variables de var */
+  /* Se renombran las variables para mejor lectura */
+  let nombre = formulario.elements[0]
+  let edad = formulario.elements[1]
+  const nacionalidadSeleccionada = formulario.elements[2]
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  //se cambia la declaracion de variables de var y se cambia el nombre
+  const nombreValor = nombre.value;
+  const edadValor = parseInt(edad.value); // Se asegura conversión a número
 
-  var nombre = n.value
-  var edad = e.value
+  /* Hay una union de variables se separan 
+  const i = nacionalidad.selectedIndexnacionalidad = nacionalidad.options[i].value
+  queda asi:
+    const i = nacionalidad.selectedIndex
+    nacionalidad = nacionalidad.options[i].value
+  */
+  const i = nacionalidadSeleccionada.selectedIndex
+  const nacionalidadValor = nacionalidadSeleccionada.options[i].value
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  console.log(nombreValor, edadValor);
+  console.log(nacionalidadValor);
 
-  if (nombre.length === 0) {
-    n.classList.add("error")
+  /* Se asignan las varibles ya declaradas anteriormente en su respectivo if */
+  if (nombreValor.length === 0) {
+    nombre.classList.add("error") // Se corrige: se usa el input
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+  if (edadValor < 18 || edadValor > 120 || isNaN(edadValor)) {
+    edad.classList.add("error") // Se corrige: se usa el input
   }
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+  if (nombreValor.length > 0
+    && (edadValor >= 18
+      && edadValor <= 120)) {
+    agregarInvitado(nombreValor, edadValor, nacionalidadValor)
   }
 }
 
-var botonBorrar = document.createElement("button")
+//Se cambia la declaracion de variables var a const
+const botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+const corteLinea = document.createElement("br")
 document.body.appendChild(corteLinea)
 document.body.appendChild(botonBorrar);
 
@@ -52,46 +64,50 @@ function agregarInvitado(nombre, edad, nacionalidad) {
     nacionalidad = "Peruana"
   }
 
-var lista = document.getElementById("lista-de-invitados")
+  //Se cambia la declaracion de variables var a const
+  const lista = document.getElementById("lista-de-invitados")
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+  const elementoLista = document.createElement("div")
+  //Esta mal definido el evento added, se cambia a .add
+  elementoLista.classList.add("elemento-lista")
+  lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+  const spanNombre = document.createElement("span")
+  const inputNombre = document.createElement("input")
+  const espacio = document.createElement("br")
+  spanNombre.textContent = "Nombre: "
+  inputNombre.value = nombre
+  elementoLista.appendChild(spanNombre)
+  elementoLista.appendChild(inputNombre)
+  elementoLista.appendChild(espacio)
 
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
+  function crearElemento(descripcion, valor) {
+    const spanNombre = document.createElement("span")
+    const inputNombre = document.createElement("input")
+    const espacio = document.createElement("br")
+    spanNombre.textContent = descripcion + ": "
+    inputNombre.value = valor
+    elementoLista.appendChild(spanNombre)
+    elementoLista.appendChild(inputNombre)
+    elementoLista.appendChild(espacio)
+  }
 
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
+  crearElemento("Nombre", nombre)
+  crearElemento("Edad", edad)
+  crearElemento("Nacionalidad", nacionalidad)
 
+  // Se cambia la declaracion de variable
+  const botonBorrar = document.createElement("button")
+  botonBorrar.textContent = "Eliminar invitado"
+  botonBorrar.id = "boton-borrar"
+  // Se cambia la declaracion de variable
+  const corteLinea = document.createElement("br")
+  elementoLista.appendChild(corteLinea)
+  elementoLista.appendChild(botonBorrar);
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
-
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
+  /* Quito las // de la linea 101 para comprobar funcionamiento */
+  botonBorrar.onclick = function () {
+    this.parentNode.style.display = 'none';
+    botonBorrar.parentNode.remove()
   }
 }
